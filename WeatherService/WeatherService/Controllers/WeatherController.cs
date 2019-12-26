9fec9a4e713f3f224fcf70824975da670d9a3d98
dislepsia <adrianlazarte@live.com.ar>
2019-12-25 22:25:36 -0300

    Se agrego el perfil de publicación de Azure Se refactorizar el Controlador

--- WeatherService/WeatherService/Controllers/WeatherController.cs
+++ WeatherService/WeatherService/Controllers/WeatherController.cs
@@ -13,11 +13,11 @@ using MongoDB.Bson.Serialization.Serializers;
 
 namespace WeatherService.Controllers
 {
-    public class MainController : ApiController
+    public class WeatherController : ApiController
     {
         WeatherReport report = null;
 
-        public MainController()
+        public WeatherController()
         {
             try
             {
@@ -26,7 +26,7 @@ namespace WeatherService.Controllers
                 IMongoDatabase db = client.GetDatabase("TestMeLi");
                 IMongoCollection<WeatherReport> pronostico = db.GetCollection<WeatherReport>("Pronostico");
 
-                FilterDefinition<WeatherReport> filters = Builders<WeatherReport>.Filter.Eq("_id", new ObjectId("5e0390e96f8924822ff7fb83"));
+                FilterDefinition<WeatherReport> filters = Builders<WeatherReport>.Filter.Eq("_id", new ObjectId("5e040a626f8924822f01df8f"));
                 report = pronostico.Find(filters).First();
 
                 //var idString = "5e015f386f8924822fc8a120";
